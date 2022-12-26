874883484a741d1329d0657dc8ba9636c80b6a90
Алёна Алёна <alenchaeto1@outlook.com>
2022-12-26 16:53:46 +0300

    Добавьте файлы проекта.

---
+++ Task11/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double k = Convert.ToDouble(Console.ReadLine());
+            double b = Convert.ToDouble(Console.ReadLine());
+
+            Equation equation = new Equation(k, b);
+            Console.WriteLine(equation.Root());
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Task11/StructTask11.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design.Serialization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task11
+{
+    struct Equation
+    {
+        double k;
+        double b;
+
+        public Equation(double k, double b)
+        {
+            this.k = k;
+            this.b = b;
+        }
+
+        public string Root()
+        {
+
+            if (k == 0 && b == 0)
+                 return "Решение - любое число"; 
+            else if (k == 0)
+                return "Решений нет";
+            return $"Решение - {-b / k}";
+        }
+    }
+}
